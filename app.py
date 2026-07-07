768906ce83987f88cd5c69b9db65c9fb68fa8b80
DeviWilk3747 <devi.wilkerson@gmail.com>
2026-07-07 16:00:32 -0500

    Add edit application feature

--- app.py
+++ app.py
@@ -92,6 +92,68 @@ def delete_application(application_id):
     flash("Application deleted successfully.")
     return redirect(url_for("view_applications"))
 
+@app.route("/edit-application/<int:application_id>", methods=["GET", "POST"])
+def edit_application(application_id):
+    # Retrieves submitted data
+    if request.method == "POST":
+        company = request.form.get("company", "").strip()
+        job_title = request.form.get("job_title", "").strip()
+        location = request.form.get("location", "").strip()
+        job_link = request.form.get("job_link", "").strip()
+        date_applied = request.form.get("date_applied", "").strip()
+        status = request.form.get("status", "").strip()
+        follow_up_date = request.form.get("follow_up_date", "").strip()
+        notes = request.form.get("notes", "").strip()
+
+        # Stops if any required field is empty
+        if not company or not job_title or not date_applied or not status:
+            return render_template(
+                "edit_application.html",
+                error="All fields required.",
+                application=(application_id, company, job_title, date_applied, status)
+            )
+        
+        cursor.execute(
+            """
+            UPDATE applications
+            SET
+                company = ?,
+                job_title = ?,
+                location = ?,
+                job_link = ?,
+                date_applied = ?,
+                status = ?,
+                follow_up_date = ?,
+                notes = ?
+            WHERE id = ?
+            """,
+            (company, job_title, location, job_link, date_applied, status, follow_up_date, notes, application_id)
+        )
+
+        connection.commit()
+        flash("Application updated successfully")
+        return redirect(url_for("view_applications"))
+    
+    # A GET request retrieves the current application for the form
+    if request.method == "GET":
+        cursor.execute(
+            """
+            SELECT * FROM applications
+            WHERE id = ?
+            """,
+            (application_id,)
+        )
+
+        application = cursor.fetchone()
+
+        # Handles an ID that does not exist
+        if application is None:
+            return "Application not found", 404
+
+    # Send selected expense to edit form    
+    return render_template("edit_application.html", application=application)
+
+
 # Run Flask server
 if __name__ == "__main__":
     app.run(debug=True)
